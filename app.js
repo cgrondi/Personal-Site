297843c5f39a7e9e7edcb2f539823cda9e3acdcd
Cameron Grondines <camerongrondines@Camerons-MacBook-Pro.local>
2021-02-15 22:22:09 -0600

    Add portfolio page. Update CSS.

--- app.js
+++ app.js
@@ -3,10 +3,38 @@ const app = express();
 app.set('view engine', 'ejs');
 app.use(express.static("public"));
 
+
+
+
+
+const projects = [
+  {
+    imgURL:"/images/NoteTaker.png",
+    title:'Note-Taker',
+    description:'A web app that allows the user to write notes down. Built using React.js',
+    codeURL:'https://github.com/cgrondi/Note-Taker',
+    demoURL:'#'
+  },
+  {
+    imgURL:'#',
+    title:'TESTING',
+    description:'A SECOND PROJECT THAT DOES NOT EXIST TO TEST THE FOREACH FUNCTION IN PORTFOLIO.EJS. DELETE AFTER TESTING.',
+    codeURL:'#',
+    demoURL:'#'
+  }
+];
+
+
 app.get('/', function(req,res){
-  const date = new Date();
-  const year = date.getFullYear();
-  res.render('index', {year: year});
+
+  res.render('index');
+});
+app.get('/portfolio', function(req,res){
+  res.render('portfolio',{projects: projects});
+});
+app.get('/about', function(req,res){
+  //temporarily until I create the about page
+  res.redirect('/');
 });
 
 app.listen(3000,function(){
